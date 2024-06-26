bdf8a574f84f258a9f9bb383adf98010640a2490
Arthur Limpens <arthur.limpens@gmail.com>
2024-06-26 14:13:16 +0200

    Half of DDA norminetted

--- src/DDA_helper.c
+++ src/DDA_helper.c
@@ -6,7 +6,7 @@
 /*   By: alimpens <alimpens@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 11:35:09 by ohoro             #+#    #+#             */
-/*   Updated: 2024/06/26 11:38:17 by alimpens         ###   ########.fr       */
+/*   Updated: 2024/06/26 14:11:26 by alimpens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,15 +45,26 @@ void	dda_init_for_horizontal(t_dda *dda, t_game *game, int i)
 	dda->found_horz_wall_hit = false;
 	dda->horz_wall_hit_x = 0;
 	dda->horz_wall_hit_y = 0;
-	dda->horz_wall_content = 0;  
+	dda->horz_wall_content = 0;
 	dda->yintercept = floor(game->player_y / TILE_SIZE) * TILE_SIZE;
-	dda->yintercept += dda->is_ray_facing_down ? TILE_SIZE : 0;
-	dda->xintercept = game->player_x + (dda->yintercept - game->player_y) / tan( game->rays[i].ray_angle);
+	if (dda->is_ray_facing_down)
+		dda->yintercept += TILE_SIZE;
+	dda->xintercept = game->player_x + 
+		(dda->yintercept - game->player_y) / tan(game->rays[i].ray_angle);
 	dda->ystep = TILE_SIZE;
-	dda->ystep *= dda->is_ray_facing_up ? -1 : 1;
-	dda->xstep = TILE_SIZE / tan( game->rays[i].ray_angle);
-	dda->xstep *= (dda->is_ray_facing_left && dda->xstep > 0) ? -1 : 1;
-	dda->xstep *= (dda->is_ray_facing_right && dda->xstep < 0) ? -1 : 1;
+	if (dda->is_ray_facing_up)
+	{
+		dda->ystep *= -1;
+	}
+	dda->xstep = TILE_SIZE / tan(game->rays[i].ray_angle);
+	if (dda->is_ray_facing_left && dda->xstep > 0)
+	{
+		dda->xstep *= -1;
+	}
+	if (dda->is_ray_facing_right && dda->xstep < 0)
+	{
+		dda->xstep *= -1;
+	}
 	dda->next_horz_touch_x = dda->xintercept;
 	dda->next_horz_touch_y = dda->yintercept;
 }
@@ -65,44 +76,123 @@ void	dda_init_for_vertical(t_dda *dda, t_game *game, int i)
 	dda->vert_wall_hit_y = 0;
 	dda->vert_wall_content = 0;
 	dda->xintercept = floor(game->player_x / TILE_SIZE) * TILE_SIZE;
-	dda->xintercept += dda->is_ray_facing_right ? TILE_SIZE : 0;
-	dda->yintercept = game->player_y + (dda->xintercept - game->player_x) * tan( game->rays[i].ray_angle);
+	if (dda->is_ray_facing_right)
+		dda->xintercept += TILE_SIZE;
+	dda->yintercept = game->player_y + (dda->xintercept - game->player_x) 
+		* tan(game->rays[i].ray_angle);
 	dda->xstep = TILE_SIZE;
-	dda->xstep *= dda->is_ray_facing_left ? -1 : 1;
-	dda->ystep = TILE_SIZE * tan( game->rays[i].ray_angle);
-	dda->ystep *= (dda->is_ray_facing_up && dda->ystep > 0) ? -1 : 1;
-	dda->ystep *= (dda->is_ray_facing_down && dda->ystep < 0) ? -1 : 1;
+	if (dda->is_ray_facing_left)
+		dda->xstep *= -1;
+	else
+		dda->xstep *= 1;
+	dda->ystep = TILE_SIZE * tan(game->rays[i].ray_angle);
+	if (dda->is_ray_facing_up && dda->ystep > 0)
+		dda->ystep *= -1;
+	else if (dda->is_ray_facing_down && dda->ystep < 0)
+		dda->ystep *= -1;
+	else
+		dda->ystep *= 1;
 	dda->next_vert_touch_x = dda->xintercept;
 	dda->next_vert_touch_y = dda->yintercept;
 }
 
+void	handle_wall_hit(t_game *game, t_dda *dda, 
+	float x_to_check, float y_to_check)
+{
+	if (has_wall_at(*game, x_to_check, y_to_check))
+	{
+		// Found a wall hit
+		dda->horz_wall_hit_x = dda->next_horz_touch_x;
+		dda->horz_wall_hit_y = dda->next_horz_touch_y;
+		dda->horz_wall_content = game->map_grid_2d[(int)floor(y_to_check / 
+				TILE_SIZE)][(int)floor(x_to_check / TILE_SIZE)];
+		dda->found_horz_wall_hit = true;
+	}
+	else
+	{
+		dda->next_horz_touch_x += dda->xstep;
+		dda->next_horz_touch_y += dda->ystep;
+	}
+}
+
 void	dda_loop_horizontal(t_game *game, t_dda *dda)
 {
-while (dda->next_horz_touch_x >= 0 && dda->next_horz_touch_x <= game->map_cols * TILE_SIZE && dda->next_horz_touch_y >= 0 && dda->next_horz_touch_y <= game->map_rows * TILE_SIZE)
-	{
-		float x_to_check = dda->next_horz_touch_x;
-		float y_to_check = dda->next_horz_touch_y + (dda->is_ray_facing_up ? -1 : 0);
+	float	x_to_check;
+	float	y_to_check;
 
-		if (has_wall_at(*game, x_to_check, y_to_check))
+	while (dda->next_horz_touch_x >= 0 && 
+		dda->next_horz_touch_x <= game->map_cols * TILE_SIZE && 
+		dda->next_horz_touch_y >= 0 && 
+		dda->next_horz_touch_y <= game->map_rows * TILE_SIZE)
+	{
+		x_to_check = dda->next_horz_touch_x;
+		if (dda->is_ray_facing_up)
 		{
-			// found a wall hit
-			dda->horz_wall_hit_x = dda->next_horz_touch_x;
-			dda->horz_wall_hit_y = dda->next_horz_touch_y;
-			dda->horz_wall_content = game->map_grid_2d[(int)floor(y_to_check / TILE_SIZE)][(int)floor(x_to_check / TILE_SIZE)];
-			dda->found_horz_wall_hit = true;
-			break ;
+			y_to_check = dda->next_horz_touch_y - 1;
 		}
 		else
 		{
-			dda->next_horz_touch_x += dda->xstep;
-			dda->next_horz_touch_y += dda->ystep;
+			y_to_check = dda->next_horz_touch_y;
 		}
+		handle_wall_hit(game, dda, x_to_check, y_to_check);
+		if (dda->found_horz_wall_hit)
+		{
+			break ;
+		}
+	}
+}
+
+void	handle_vertical_wall_hit(t_game *game, t_dda *dda, 
+	float x_to_check, float y_to_check)
+{
+	if (has_wall_at(*game, x_to_check, y_to_check))
+	{
+		// Found a wall hit
+		dda->vert_wall_hit_x = dda->next_vert_touch_x;
+		dda->vert_wall_hit_y = dda->next_vert_touch_y;
+		dda->vert_wall_content = game->map_grid_2d[(int)floor(y_to_check / 
+				TILE_SIZE)][(int)floor(x_to_check / TILE_SIZE)];
+		dda->found_vert_wall_hit = true;
+	}
+	else
+	{
+		dda->next_vert_touch_x += dda->xstep;
+		dda->next_vert_touch_y += dda->ystep;
 	}
 }
 
 void	dda_loop_vertical(t_game *game, t_dda *dda)
 {
-while (dda->next_vert_touch_x >= 0 && dda->next_vert_touch_x <= game->map_cols * TILE_SIZE && dda->next_vert_touch_y >= 0 && dda->next_vert_touch_y <= game->map_rows * TILE_SIZE)
+	float	x_to_check;
+	float	y_to_check;
+
+	while (dda->next_vert_touch_x >= 0 && 
+		dda->next_vert_touch_x <= game->map_cols * TILE_SIZE && 
+		dda->next_vert_touch_y >= 0 && 
+		dda->next_vert_touch_y <= game->map_rows * TILE_SIZE)
+	{
+		if (dda->is_ray_facing_left)
+		{
+			x_to_check = dda->next_vert_touch_x - 1;
+		}
+		else
+		{
+			x_to_check = dda->next_vert_touch_x;
+		}
+		y_to_check = dda->next_vert_touch_y;
+		handle_vertical_wall_hit(game, dda, x_to_check, y_to_check);
+		if (dda->found_vert_wall_hit)
+		{
+			break ;
+		}
+	}
+}
+
+/* void	dda_loop_vertical(t_game *game, t_dda *dda)
+{
+	while (dda->next_vert_touch_x >= 0 && 
+		dda->next_vert_touch_x <= game->map_cols * TILE_SIZE && 
+			dda->next_vert_touch_y >= 0 && dda->next_vert_touch_y <= game->map_rows * TILE_SIZE)
 	{
 		float x_to_check = dda->next_vert_touch_x + (dda->is_ray_facing_left ? -1 : 0);
 		float y_to_check = dda->next_vert_touch_y;
@@ -122,7 +212,7 @@ while (dda->next_vert_touch_x >= 0 && dda->next_vert_touch_x <= game->map_cols *
 			dda->next_vert_touch_y += dda->ystep;
 		}
 	}
-}
+} */
 
 void	calculate_nearest_intersection(t_game *game, t_dda *dda, int i)
 {
