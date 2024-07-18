c27c1d80d6174681e3ebee58212f3692fe4ed8ef
Arthur Limpens <arthur.limpens@gmail.com>
2024-07-18 16:46:09 +0200

    Broke up validate file

--- src/validate_file.c
+++ src/validate_file.c
@@ -6,14 +6,44 @@
 /*   By: alimpens <alimpens@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 17:44:02 by ohoro             #+#    #+#             */
-/*   Updated: 2024/07/18 16:18:39 by alimpens         ###   ########.fr       */
+/*   Updated: 2024/07/18 16:36:50 by alimpens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	validate_textures_and_colors(t_validation *validation)
+{
+	if (validation->north_texture != 1 || validation->south_texture != 1 
+		|| validation->west_texture != 1 || validation->east_texture != 1)
+	{
+		ft_error(ERR_TEX, NULL);
+	}
+	if (validation->floor_color_num != 1 || validation->ceiling_color_num != 1)
+		ft_error(ERR_COLOR, NULL); 
+}
+
+void	process_file_lines(int fd, t_validation *validation, int map_start_line)
+{
+	char	*line;
+	int		i;
+
+	i = map_start_line;
+	line = get_next_line(fd);
+	while (line && i)
+	{
+		line_check_textures(line, validation);
+		color_check(line, validation);
+		free(line);
+		line = get_next_line(fd);
+		i--;
+	}
+}
+
 void	validate_file(t_game *game, char *file, t_validation *validation)
 {
+	int	fd;
+
 	validation->map_start_line = game->map_start;
 	validation->north_texture = 0;
 	validation->south_texture = 0;
@@ -27,29 +57,8 @@ void	validate_file(t_game *game, char *file, t_validation *validation)
 	validation->c_red = 0;
 	validation->c_green = 0;
 	validation->c_blue = 0;
-
-	int		fd;
-	char	*line = NULL;
-	int		i;
-
-	i = validation->map_start_line;
 	fd = open(file, O_RDONLY);
-	line = get_next_line(fd);
-	while (line && i)
-	{
-		line_check_textures(line, validation);
-		color_check(line, validation);
-		free(line);
-		line = get_next_line(fd);
-		i--;
-	}
+	process_file_lines(fd, validation, validation->map_start_line);
 	close(fd);
-	if (validation->north_texture != 1 || validation->south_texture != 1 
-		|| validation->west_texture != 1 || validation->east_texture != 1)
-	{
-		printf("YES XXX ITS AN ERROR\n");
-		ft_error(ERR_TEX, NULL);
-	}
-	if (validation->floor_color_num != 1 || validation->ceiling_color_num != 1)
-		ft_error(ERR_COLOR, NULL); 
+	validate_textures_and_colors(validation);
 }
--- src/validate_file_b.c
+++ src/validate_file_b.c
@@ -6,7 +6,7 @@
 /*   By: alimpens <alimpens@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 13:51:53 by ohoro             #+#    #+#             */
-/*   Updated: 2024/07/18 16:20:41 by alimpens         ###   ########.fr       */
+/*   Updated: 2024/07/18 16:45:39 by alimpens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,10 @@ int	parse_color_value(char *line, int *i)
 
 	value = ft_atoi(&line[*i]);
 	while (ft_isdigit(line[*i]))
-	{
 		(*i)++;
-	}
-	if (line[*i] != ',' && line[*i] != ' ' && line[*i] != '\t' && line[*i] != '\n' && line[*i] != '\0')
-	{
+	if (line[*i] != ',' && line[*i] != ' ' && line[*i] != '\t' 
+		&& line[*i] != '\n' && line[*i] != '\0')
 		ft_error(ERR_COLOR_VAL, NULL);
-	}
 	return (value);
 }
 
@@ -32,25 +29,27 @@ void	parse_color_line_f(char *line, int *i, t_validation *v)
 {
 	ft_skip_spaces(line, i);
 	v->f_red = parse_color_value(line, i);
-	if (line[*i] == ',')(*i)++;
-		v->f_green = parse_color_value(line, i);
-	if (line[*i] == ',')(*i)++;
-		v->f_blue = parse_color_value(line, i);
+	if (line[*i] == ',')
+		(*i)++;
+	v->f_green = parse_color_value(line, i);
+	if (line[*i] == ',')
+		(*i)++;
+	v->f_blue = parse_color_value(line, i);
 	ft_skip_spaces(line, i);
 	if (line[*i] != '\n')
-	{
 		ft_error(ERR_COLOR_NUM, NULL);
-	}
 }
 
 void	parse_color_line_c(char *line, int *i, t_validation *v)
 {
 	ft_skip_spaces(line, i);
-		v->c_red = parse_color_value(line, i);
-	if (line[*i] == ',')(*i)++;
-		v->c_green = parse_color_value(line, i);
-	if (line[*i] == ',')(*i)++;
-		v->c_blue = parse_color_value(line, i);
+	v->c_red = parse_color_value(line, i);
+	if (line[*i] == ',')
+		(*i)++;
+	v->c_green = parse_color_value(line, i);
+	if (line[*i] == ',')
+		(*i)++;
+	v->c_blue = parse_color_value(line, i);
 	ft_skip_spaces(line, i);
 	if (line[*i] != '\n')
 	{
@@ -64,7 +63,6 @@ void	color_check(char *line, t_validation *v)
 
 	i = 0;
 	ft_skip_spaces(line, &i);
-
 	if (line[i] == 'F')
 	{
 		i++;
@@ -75,11 +73,10 @@ void	color_check(char *line, t_validation *v)
 		i++;
 		parse_color_line_c(line, &i, v);
 	}
-	if (v->f_red < 0 || v->f_red > 255 || v->f_green < 0 || v->f_green > 255 || v->f_blue < 0 || v->f_blue > 255)
-	{
-		ft_error(ERR_COLOR_VAL, NULL);
-	}
-	if (v->c_red < 0 || v->c_red > 255 || v->c_green < 0 || v->c_green > 255 || v->c_blue < 0 || v->c_blue > 255)
+	if (v->f_red < 0 || v->f_red > 255 || v->f_green < 0 
+		|| v->f_green > 255 || v->f_blue < 0 || v->f_blue > 255 
+		|| v->c_red < 0 || v->c_red > 255 || v->c_green < 0 
+		|| v->c_green > 255 || v->c_blue < 0 || v->c_blue > 255)
 	{
 		ft_error(ERR_COLOR_VAL, NULL);
 	}
@@ -93,23 +90,22 @@ void	line_check_textures(char *line, t_validation *validation)
 	while (line[i] != '\n' && line[i] != '\0')
 	{
 		if (line[i] == ' ' || line[i] == '\t')
+		{
 			i++;
-		if ((line[i] == 'N' && line[i +1] == 'O'))
+			continue ;
+		}
+		if (line[i] == 'N' && line[i + 1] == 'O')
 			validation->north_texture += 1;
-		else if (line[i] == 'S' && line[i +1] == 'O')
+		else if (line[i] == 'S' && line[i + 1] == 'O')
 			validation->south_texture += 1;
-		else if (line[i] == 'W' && line[i +1] == 'E')
+		else if (line[i] == 'W' && line[i + 1] == 'E')
 			validation->west_texture += 1;
-		else if (line[i] == 'E' && line[i +1] == 'A')
+		else if (line[i] == 'E' && line[i + 1] == 'A')
 			validation->east_texture += 1;
 		else if (line[i] == 'F')
-		{
 			validation->floor_color_num = 1;
-		}
 		else if (line[i] == 'C')
-		{
 			validation->ceiling_color_num = 1;
-		}
 		i++;
 	}
 }
